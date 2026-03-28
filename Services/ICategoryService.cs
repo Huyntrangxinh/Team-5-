533dcd85724ca7c2bb338ea9c6b5612bf20328a6
Aeron <aeronbtw@gmail.com>
2026-03-28 19:43:20 +0700

    refactor(#40): simplify ICategoryService - keep only filter methods

--- Services/ICategoryService.cs
+++ Services/ICategoryService.cs
@@ -1,11 +1,9 @@
+using Campus.Models;
+
 namespace Campus.Services;
 
 public interface ICategoryService
 {
     Task<List<Category>> GetAllCategoriesAsync();
-    Task<Category?> GetCategoryByIdAsync(int id);
     Task<List<Category>> GetCategoriesByTypeAsync(CategoryType type);
-    Task AddCategoryAsync(Category category);
-    Task UpdateCategoryAsync(Category category);
-    Task DeleteCategoryAsync(int id);
 }
