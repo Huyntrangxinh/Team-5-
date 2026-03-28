65fd134b2b5f771ced8479b3a5e0c186faa9f483
LeThZ25 <duonglethuong2005@gmail.com>
2026-03-28 18:21:45 +0700

    Basic Servives with Seed data

---
+++ Services/IMyEventService.cs
@@ -0,0 +1,10 @@
+﻿using Campus_Activity_Manager.Models;
+
+namespace Campus_Activity_Manager.Services
+{
+	public interface IMyEventService
+	{
+		Task<List<Event>> GetMyEventsAsync();
+		Task<bool> UnregisterEventAsync(Guid eventId);
+	}
+}
