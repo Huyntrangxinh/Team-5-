fe91aa347f02d59dfc16f7c48965db20a3c301b3
Minh <leeanhminhw@gmail.com>
2026-03-29 18:21:09 +0700

    Create LoginViewModel base

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Campus.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
