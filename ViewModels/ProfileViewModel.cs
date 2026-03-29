1e860c4d0856acf3b37dfe8bf0a5523d188d7aa0
MaTuanKhoa <makhoa05@gmail.com>
2026-03-29 11:04:59 +0700

    feat(profile): add user properties and bind to UI

--- ViewModels/ProfileViewModel.cs
+++ ViewModels/ProfileViewModel.cs
@@ -1,15 +1,36 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace Campus.ViewModels
-{
-    public class ProfileViewModel : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
+namespace Campus.ViewModels;
 
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
+public class ProfileViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    private string _email;
+    public string Email
+    {
+        get => _email;
+        set
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            _email = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _fullName;
+    public string FullName
+    {
+        get => _fullName;
+        set
+        {
+            _fullName = value;
+            OnPropertyChanged();
         }
     }
 }
