8f5dfda484c65b7d9a99d616c640cd301b499fb7
MaTuanKhoa <makhoa05@gmail.com>
2026-03-29 09:31:54 +0700

    create ProfileViewModel base structure

---
+++ ViewModels/ProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Campus.ViewModels
+{
+    public class ProfileViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
